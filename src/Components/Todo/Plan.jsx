dcd5aba278ac5ccd9460c2c51dc07a3b8d4816ff
Kamrul Hasan <kamrul.h456@gmail.com>
2022-12-16 18:05:21 +0600

    Upload

--- src/Components/Todo/Plan.jsx
+++ src/Components/Todo/Plan.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Plan = props => {
     return (
         <div className="row">
-            <li>{props.item}</li>
+            <li className="shadow p-2 my-2 col-sm-9">{props.item}</li>
+            <button onClick={() => { props.sendData(props.id) }} className="btn btn-danger my-2 col-sm-2 offset-1">X</button>
         </div>
     );
 }
--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -8,6 +8,17 @@ class Todo extends React.Component {
         text: "",
     }
 
+    handleChangeFn = val => {
+        this.setState({ text: val.target.value });
+    }
+
+    handleAddFn = () => {
+        if (this.state.text !== "") {
+            const items = [...this.state.items, this.state.text];
+            this.setState({ items: items, text: "" });
+        }
+    }
+
     handleChange = e => {
         this.setState({ text: e.target.value });
     }
@@ -15,13 +26,24 @@ class Todo extends React.Component {
         if (this.state.text !== "") {
             const items = [...this.state.items, this.state.text];
             this.setState({ items: items, text: "" });
-            console.log(this.state.items);
         }
     }
 
+    handleDelete = id => {
+
+        console.log("Deleted", id);
+        const oldItems = [...this.state.items];
+        // console.log('oldItems', oldItems)
+        const items = oldItems.filter((element, i) => {
+            return i !== id
+        });
+        // console.log("new Items", items);
+        this.setState({ items: items });
+    }
+
     render() {
-        const items = this.state.items.map(plan => {
-            return <Plan item={plan} key={Math.random()} />
+        const items = this.state.items.map((plan, i) => {
+            return <Plan item={plan} id={i} sendData={this.handleDelete} key={Math.random()} />
         })
         return (
             <div className="container">
@@ -29,14 +51,15 @@ class Todo extends React.Component {
                     <div className="col-md-6 col-sm-12 mx-auto">
                         <h3 className="text-center">Add Todo</h3>
                         <hr />
-                        <div className="row">
+                        {/* <div className="row">
                             <div className="col-9 mx-auto">
                                 <input onChange={this.handleChange} type="text" className="form-control" placeholder="What To do" value={this.state.text} />
                             </div>
                             <div className="col-3">
                                 <button onClick={this.handleAdd} className="btn btn-info px-3 font-weight-bold">Add</button>
                             </div>
-                        </div>
+                        </div> */}
+                        <AddTodo changeFn={this.handleChangeFn} addFn={this.handleAddFn} val={this.state.text} />
                     </div>
                     <div className="col-md-6 col-sm-12 mx-auto">
                         <h3 className="text-center">Todays Work</h3>
